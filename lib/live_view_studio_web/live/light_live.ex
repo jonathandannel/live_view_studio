cfa87b31b3c784aa0ed5c8b41096fd93bdadfb87
Jonathan Dannel <jdannel@adobe.com>
2023-12-24 07:56:08 -0500

    add new live route for light component

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,16 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <%= @brightness %>
+    </div>
+    """
+  end
+end
